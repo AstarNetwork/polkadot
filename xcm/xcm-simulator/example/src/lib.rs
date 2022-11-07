b9382b1338c71121ef15a6d3d4c20cff67bf2de5
Dinonard <dino.pacandi@gmail.com>
2022-11-07 13:16:16 +0100

    Init commit with experiments

--- xcm/xcm-simulator/example/src/lib.rs
+++ xcm/xcm-simulator/example/src/lib.rs
@@ -21,6 +21,7 @@ use frame_support::sp_tracing;
 use xcm::prelude::*;
 use xcm_executor::traits::Convert;
 use xcm_simulator::{decl_test_network, decl_test_parachain, decl_test_relay_chain};
+// use xcm::latest::WildFungibility;
 
 pub const ALICE: sp_runtime::AccountId32 = sp_runtime::AccountId32::new([0u8; 32]);
 pub const INITIAL_BALANCE: u128 = 1_000_000_000;
@@ -267,6 +268,136 @@ mod tests {
 		});
 	}
 
+	//////////////////////////////////////////////////////
+	///////////////// SCENARIOS START/////////////////////
+	//////////////////////////////////////////////////////
+
+	/// Scenario:
+	///
+	/// Original:
+	/// A parachain wants to be notified that a transfer worked correctly.
+	/// It sends a `QueryHolding` after the deposit to get notified on success.
+	///
+	/// Modified:
+	/// The example has been modified slightly to demonstrate that correct asset amount is returned.
+	/// We withdraw certain amount, but deposit less. We expect that `QueryHolding` will report the remainder.
+	///
+	/// Asserts that the balances are updated correctly and the expected XCM is sent.
+	#[test]
+	fn query_holding() {
+		MockNet::reset();
+
+		let withdraw_amount = 10;
+		let deposit_amount = 3;
+		let first_query_id_set = 1234;
+		let second_query_id_set = 5678;
+
+		// Send a message which fully succeeds on the relay chain
+		ParaA::execute_with(|| {
+			let message = Xcm(vec![
+				WithdrawAsset((Here, withdraw_amount).into()),
+				// We don't deposit everything intentionally, so we can check that `ReportHolding` works as expected
+				DepositAsset {
+					assets: MultiAsset { id: Concrete(Here.into()), fun: Fungible(deposit_amount) }
+						.into(),
+					beneficiary: Parachain(2).into(),
+				},
+				ReportHolding {
+					response_info: QueryResponseInfo {
+						destination: Parachain(1).into(),
+						query_id: first_query_id_set,
+						max_weight: 1_000_000_000,
+					},
+					assets: All.into(), // we choose to report everything, but we could limit it if we wanted to
+				},
+				ReportHolding {
+					response_info: QueryResponseInfo {
+						destination: Parachain(1).into(),
+						query_id: second_query_id_set,
+						max_weight: 1_000_000_000,
+					},
+					// We repeat almost the same query but we limit it to only native asset and to 1 amount!
+					// So even if we have more than 1, at max, 1 can be reported (0 or 1).
+					assets: MultiAsset { id: Concrete(Here.into()), fun: Fungible(1) }.into(),
+				},
+			]);
+			// Send withdraw and deposit with query holding
+			assert_ok!(ParachainPalletXcm::send_xcm(Here, Parent, message.clone(),));
+		});
+
+		// Check that transfer was executed
+		Relay::execute_with(|| {
+			// Withdraw executed
+			assert_eq!(
+				relay_chain::Balances::free_balance(child_account_id(1)),
+				INITIAL_BALANCE - withdraw_amount
+			);
+			// Deposit executed
+			assert_eq!(
+				relay_chain::Balances::free_balance(child_account_id(2)),
+				INITIAL_BALANCE + deposit_amount
+			);
+		});
+
+		// Check that QueryResponse message was received
+		ParaA::execute_with(|| {
+			assert_eq!(
+				parachain::MsgQueue::received_dmp(),
+				vec![
+					Xcm(vec![QueryResponse {
+						query_id: first_query_id_set,
+						response: Response::Assets(
+							(Parent, withdraw_amount - deposit_amount).into()
+						),
+						max_weight: 1_000_000_000,
+						querier: Some(Here.into()),
+					},]),
+					Xcm(vec![QueryResponse {
+						query_id: second_query_id_set,
+						response: Response::Assets((Parent, 1).into()),
+						max_weight: 1_000_000_000,
+						querier: Some(Here.into()),
+					},])
+					// Notice that we define 2 separate XCM sequences. This is because each `QueryHolding`'s reponse is done individually.
+				],
+			);
+
+			// TODO: check what happened with the unused funds left in the holding register
+		});
+	}
+
+	/// Scenario:
+	/// A parachain transfers an NFT resident on the relay chain to another parachain account.
+	///
+	/// Asserts that the parachain accounts are updated as expected.
+	#[test]
+	fn withdraw_and_deposit_nft() {
+		MockNet::reset();
+
+		Relay::execute_with(|| {
+			assert_eq!(relay_chain::Uniques::owner(1, 42), Some(child_account_id(1)));
+		});
+
+		ParaA::execute_with(|| {
+			// We want to transfer asset owned by `ParaA` over to `Para2`
+			let message = Xcm(vec![TransferAsset {
+				assets: (GeneralIndex(1), 42u32).into(),
+				beneficiary: Parachain(2).into(),
+			}]);
+			// We send the message to the `Parent`, the relay chain, since asset is there
+			assert_ok!(ParachainPalletXcm::send_xcm(Here, Parent, message));
+		});
+
+		// Asset ownership has changed
+		Relay::execute_with(|| {
+			assert_eq!(relay_chain::Uniques::owner(1, 42), Some(child_account_id(2)));
+		});
+	}
+
+	//////////////////////////////////////////////////////
+	///////////////// SCENARIOS END //////////////////////
+	//////////////////////////////////////////////////////
+
 	#[test]
 	fn remote_locking() {
 		MockNet::reset();
@@ -301,32 +432,6 @@ mod tests {
 					asset: (Parent, locked_amount).into()
 				}])]
 			);
-		});
-	}
-
-	/// Scenario:
-	/// A parachain transfers an NFT resident on the relay chain to another parachain account.
-	///
-	/// Asserts that the parachain accounts are updated as expected.
-	#[test]
-	fn withdraw_and_deposit_nft() {
-		MockNet::reset();
-
-		Relay::execute_with(|| {
-			assert_eq!(relay_chain::Uniques::owner(1, 42), Some(child_account_id(1)));
-		});
-
-		ParaA::execute_with(|| {
-			let message = Xcm(vec![TransferAsset {
-				assets: (GeneralIndex(1), 42u32).into(),
-				beneficiary: Parachain(2).into(),
-			}]);
-			// Send withdraw and deposit
-			assert_ok!(ParachainPalletXcm::send_xcm(Here, Parent, message));
-		});
-
-		Relay::execute_with(|| {
-			assert_eq!(relay_chain::Uniques::owner(1, 42), Some(child_account_id(2)));
 		});
 	}
 
@@ -560,65 +665,6 @@ mod tests {
 			assert_eq!(
 				relay_chain::Balances::free_balance(child_account_id(2)),
 				INITIAL_BALANCE + send_amount
-			);
-		});
-	}
-
-	/// Scenario:
-	/// A parachain wants to be notified that a transfer worked correctly.
-	/// It sends a `QueryHolding` after the deposit to get notified on success.
-	///
-	/// Asserts that the balances are updated correctly and the expected XCM is sent.
-	#[test]
-	fn query_holding() {
-		MockNet::reset();
-
-		let send_amount = 10;
-		let query_id_set = 1234;
-
-		// Send a message which fully succeeds on the relay chain
-		ParaA::execute_with(|| {
-			let message = Xcm(vec![
-				WithdrawAsset((Here, send_amount).into()),
-				buy_execution((Here, send_amount)),
-				DepositAsset { assets: AllCounted(1).into(), beneficiary: Parachain(2).into() },
-				ReportHolding {
-					response_info: QueryResponseInfo {
-						destination: Parachain(1).into(),
-						query_id: query_id_set,
-						max_weight: 1_000_000_000,
-					},
-					assets: All.into(),
-				},
-			]);
-			// Send withdraw and deposit with query holding
-			assert_ok!(ParachainPalletXcm::send_xcm(Here, Parent, message.clone(),));
-		});
-
-		// Check that transfer was executed
-		Relay::execute_with(|| {
-			// Withdraw executed
-			assert_eq!(
-				relay_chain::Balances::free_balance(child_account_id(1)),
-				INITIAL_BALANCE - send_amount
-			);
-			// Deposit executed
-			assert_eq!(
-				relay_chain::Balances::free_balance(child_account_id(2)),
-				INITIAL_BALANCE + send_amount
-			);
-		});
-
-		// Check that QueryResponse message was received
-		ParaA::execute_with(|| {
-			assert_eq!(
-				parachain::MsgQueue::received_dmp(),
-				vec![Xcm(vec![QueryResponse {
-					query_id: query_id_set,
-					response: Response::Assets(MultiAssets::new()),
-					max_weight: 1_000_000_000,
-					querier: Some(Here.into()),
-				}])],
 			);
 		});
 	}
